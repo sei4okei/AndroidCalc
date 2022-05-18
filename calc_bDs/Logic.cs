c22811a0affead1a533587d12c3aab1d5a8d39a6
Daniil <92815326+sei4okei@users.noreply.github.com>
2022-05-18 08:39:53 +0300

    Add files via upload

---
+++ calc_bDs/Logic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace calc_bDs
+{
+    internal class Logic
+    {
+        public static void DefineButton(object sender, EventArgs e)
+        {
+            switch ((sender as Button).Text)
+            {
+                case "+":
+
+                    break;
+            }
+        }
+    }
+}
---
+++ calc_bDs/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace calc_bDs
+{
+    public partial class MainPage : ContentPage
+    {
+        string[] TextButtons = new string[19] {"c", "del", "%", "/",
+                                                "7", "8", "9", "*",
+                                                "4", "5", "6", "-",
+                                                "1", "2", "3", "+",
+                                                 "0", ",", "="};
+
+        public MainPage()
+        {
+            int row = 1; int column = 0;
+            Grid grid = new Grid();
+            var ResultView = new Label
+            {
+                Text = "Рабotает!!",
+                FontSize = 38,
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalTextAlignment = TextAlignment.End
+            };
+            Grid.SetRow(ResultView, 0);
+            Grid.SetColumn(ResultView, 0);
+            Grid.SetColumnSpan(ResultView, 4);
+            grid.Children.Add(ResultView);
+
+            for (int i = 0; i < TextButtons.Length; i++)
+            {
+                Button FuncButton = new Button
+                {
+                    Text = TextButtons[i]
+                };
+                FuncButton.Clicked += Logic.DefineButton;
+                Grid.SetRow(FuncButton, row);
+                Grid.SetColumn(FuncButton, column);
+                grid.Children.Add(FuncButton);
+                column++;
+                if (column > 3)
+                {
+                    column = 0;
+                    row++;
+                }
+                if (column == 0 && row == 5)
+                {
+                    column++;
+                }
+            }
+
+            this.Content = grid;
+        }
+    }
+}
